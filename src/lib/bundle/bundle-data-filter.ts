1bb5ab689041a5fdfe0bce85ece9fddaed04daf1
achugaev <achugaev93@gmail.com>
2021-03-08 14:01:19 +0200

    fix(bundle): add missing JSDoc

--- src/lib/bundle/bundle-data-filter.ts
+++ src/lib/bundle/bundle-data-filter.ts
@@ -2,6 +2,11 @@ import { Injectable } from '@angular/core';
 import { PhraseKey } from '../phrase/phrase-key';
 import { BundleData } from './bundle-data';
 
+/**
+ * Represents a filter of bundle data.
+ * @since 2.0.0
+ * @internal
+ */
 @Injectable({
   providedIn: 'root',
 })
